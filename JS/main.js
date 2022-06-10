21fdb71cb43a32d4e1fa39de0a6cd2df0573de3e
norellag2 <norellag2@gmail.com>
2022-06-10 11:20:39 +0200

    proyecto2

---
+++ JS/main.js
@@ -0,0 +1,27 @@
+let primeraSeccion = document.getElementById('primera');
+
+let header = document.querySelector('header');
+
+if(primeraSeccion) {
+  window.addEventListener('scroll',() => {
+    let positionScroll = window.scrollY;
+    let positionPrimeraSeccion = primeraSeccion.getBoundingClientRect().top;
+  if (positionScroll < (positionPrimeraSeccion + positionScroll)) {
+      header.classList.add('transparent');
+  } else {
+     header.classList.remove('transparent');
+  }
+})
+}
+
+
+document.querySelector('.copyright').innerHTML = '&copy; ' + new Date().getFullYear() + 'ACME S.A.';
+
+
+let aside = document.querySelector ('aside');
+let burger = document.querySelector ('.burger');
+
+burger.addEventListener('click', () => {
+  aside.classList.toggle('abierto');
+  burger.classList.toggle('abierto');
+})
